239bcd79108a3600783001816de4fe8879c9ed09
kamalshree <whatsupcoders@gmail.com>
2019-06-06 22:39:14 -0500

    Charts Implementation

---
+++ lib/main.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xffff6101),
+      ),
+      home: MyHomePage(title: 'Sales Dashboard 2018'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
+  var data1 = [0.0,-2.0,3.5,-2.0,0.5,0.7,0.8,1.0,2.0,3.0,3.2];
+
+  List<CircularStackEntry> circularData = <CircularStackEntry>[
+    new CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(700.0, Color(0xff4285F4), rankKey: 'Q1'),
+        new CircularSegmentEntry(1000.0, Color(0xfff3af00), rankKey: 'Q2'),
+        new CircularSegmentEntry(1800.0, Color(0xffec3337), rankKey: 'Q3'),
+        new CircularSegmentEntry(1000.0, Color(0xff40b24b), rankKey: 'Q4'),
+      ],
+      rankKey: 'Quarterly Profits',
+    ),
+  ];
+
+  Material myTextItems(String title, String subtitle){
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child:Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+               children: <Widget>[
+
+                  Padding(
+                   padding: EdgeInsets.all(8.0),
+                      child:Text(title,style:TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),),
+                    ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child:Text(subtitle,style:TextStyle(
+                      fontSize: 30.0,
+                    ),),
+                  ),
+
+               ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Material myCircularItems(String title, String subtitle){
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child:Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+                children: <Widget>[
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child:Text(title,style:TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueAccent,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child:Text(subtitle,style:TextStyle(
+                      fontSize: 30.0,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding:EdgeInsets.all(8.0),
+                    child:AnimatedCircularChart(
+                      size: const Size(100.0, 100.0),
+                      initialChartData: circularData,
+                      chartType: CircularChartType.Pie,
+                    ),
+                  ),
+
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Material mychart1Items(String title, String priceVal,String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(title, style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueAccent,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(priceVal, style: TextStyle(
+                      fontSize: 30.0,
+                    ),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(subtitle, style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: new Sparkline(
+                      data: data,
+                      lineColor: Color(0xffff6101),
+                      pointsMode: PointsMode.all,
+                      pointSize: 8.0,
+                    ),
+                  ),
+
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Material mychart2Items(String title, String priceVal,String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(title, style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueAccent,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(priceVal, style: TextStyle(
+                      fontSize: 30.0,
+                    ),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(subtitle, style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey,
+                    ),),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: new Sparkline(
+                      data: data1,
+                      fillMode: FillMode.below,
+                      fillGradient: new LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.amber[800], Colors.amber[200]],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {
+          //
+        }),
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(
+              FontAwesomeIcons.chartLine), onPressed: () {
+            //
+          }),
+        ],
+      ),
+      body:Container(
+          color:Color(0xffE5E5E5),
+          child:StaggeredGridView.count(
+            crossAxisCount: 4,
+           crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: mychart1Items("Sales by Month","421.3M","+12.9% of target"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: myCircularItems("Quarterly Profits","68.7M"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right:8.0),
+            child: myTextItems("Mktg. Spend","48.6M"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right:8.0),
+            child: myTextItems("Users","25.5M"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: mychart2Items("Conversion","0.9M","+19% of target"),
+          ),
+
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(4, 250.0),
+          StaggeredTile.extent(2, 250.0),
+          StaggeredTile.extent(2, 120.0),
+          StaggeredTile.extent(2, 120.0),
+          StaggeredTile.extent(4, 250.0),
+        ],
+      ),
+      ),
+    );
+  }
+}
